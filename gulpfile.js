11c55eab71f89b430678da17e3bbf2f102be6ffd
StreetHoboSP <streethobosp@gmail.com>
2017-04-14 21:07:48 +0300

    Added live server to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    liveServer = require('gulp-live-server');
 
 gulp.task('default', function () {
     gulp.src('src/guitar.js')
         .pipe(uglify())
         .pipe(rename('guitar.min.js'))
         .pipe(gulp.dest('src'));
+
+    var server = liveServer.static('.', 8000);
+    server.start();
 });
